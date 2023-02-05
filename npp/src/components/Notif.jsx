9af53ee30c100fb18ac9034224a867342dcdc015
ToroDev <86387112+HecCod@users.noreply.github.com>
2023-02-05 18:23:57 -0300

    commit

--- npp/src/components/Notif.jsx
+++ npp/src/components/Notif.jsx
@@ -1,14 +1,21 @@
 import styles from "./Notif.module.css";
 
 const Notif = (props) => {
+  const postGray = "My first tournament today!";
+  postGray.fontcolor('#d3d3d3')
+
   return (
     <div className={styles.notif}>
       <div>
-        <img src={props.avatar} alt="avatarr" />
+        <img src={props.avatar} alt="avatar" />
       </div>
       <main>
         <p>
-          <strong>{props.name}</strong> {props.action} <span>{props.post}</span>
+          <strong>{props.name}</strong> {props.action}{" "}
+          <span>
+            {props.post}{" "}
+            <span className={styles.newNot}>&#128308;</span>
+          </span>
         </p>
         <p>{props.time}</p>
       </main>
